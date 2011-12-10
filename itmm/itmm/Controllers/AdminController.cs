c0b689ea8693bff0ac3ce52a2eb9f5db261662eb
jowi englis <silkherb@gmail.com>
2011-12-10 13:53:24 +0800

    inital stratup

---
+++ itmm/itmm/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace itmm.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            ViewBag.MyMessage = "Welcome to administrator page <- Message brought to you by ViewBag";
+            return View();
+        }
+
+    }
+}
